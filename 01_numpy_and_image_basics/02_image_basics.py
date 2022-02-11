e33f48a64a200abb61030a2be8d53b71b1235e17
Felix Jing <32583955+fjing1@users.noreply.github.com>
2022-02-10 21:34:45 -0700

    completed the assignment 1

--- 01_numpy_and_image_basics/02_image_basics.py
+++ 01_numpy_and_image_basics/02_image_basics.py
@@ -1,22 +1,25 @@
 # import numpy, matplotlib.pyplot and PIL's Image libs
-
-
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
 print('open image file using Image open function in pillow library')
-
-
+pic = Image.open('../data/bird.jpg')
+print(pic)
+print(type(pic))
 print('cast the image data into numpy array')
-
-
+pic_arr = np.array(pic)
+print('shape',pic_arr.shape)
 print('use plt image show function to show the image')
-
-
+plt.imshow(pic_arr)
+plt.show()
 print('copy picture array into a new variable so that we can adjust the RGB channel later on.')
-
+pic_red = pic_arr.copy()
 
 print('zero out green and blue channels\' value and only keep color value in Red channel.')
-
-
+pic_red[:,:,1] = 0 #zero out contribution from green
+pic_red[:,:,2] = 0 #red
 print('try to debug the pic_red variable and see what are the RGB value examples in 3 channels after zero out.')
-
+plt.imshow(pic_red)
+plt.show()
 
 print('show the result using imshow function')
--- 01_numpy_and_image_basics/assignment1.py
+++ 01_numpy_and_image_basics/assignment1.py
@@ -2,14 +2,25 @@
 import numpy as np
 
 # Create and print a 3 by 3 array where every number is a 15
-
+a1 = np.ones((3,3))*15
+print('a1\n',a1)
 # print out what are the largest and smalled values in the array below
 arr = np.array([[1,2,3],[4,5,6],[7,8,9]])
-
+print('largest value is', arr.max())
+print('smallest value is',arr.min())
 # import pyplot lib from matplotlib and Image lib from  PIL
+import matplotlib.pyplot as plt
+from PIL import Image
 
 # use PIL and matplotlib to read and display the ../data/zebra.jpg image
-
+pic1 =  Image.open('../data/zebra.jpg')
 # convert the image to a numpy arrary and print the shape of the arrary
-
+pic1_arr = np.array(pic1)
+print(pic1_arr.shape)
 # use slicing to set the RED and GREEN channels of the picture to 0, then use imshow() to show the isolated blue channel
+#RGB 012
+pic1_arr[:,:,0 ]=0
+pic1_arr[:,:,1 ]=0
+plt.imshow(pic1_arr)
+plt.show()
+#picture only show blue
