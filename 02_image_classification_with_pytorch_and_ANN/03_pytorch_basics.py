b3896b442ffb35c6999bc4585899a424e5875001
Felix Jing <32583955+fjing1@users.noreply.github.com>
2022-02-12 10:00:57 -0700

    completed the assignment 1

--- 02_image_classification_with_pytorch_and_ANN/03_pytorch_basics.py
+++ 02_image_classification_with_pytorch_and_ANN/03_pytorch_basics.py
@@ -3,41 +3,60 @@
 # Creating tensors from scratch
 
 # import libs torch, numpy
-
+import torch
+import numpy as np
 
 print('converting NumPy arrays to PyTorch tensors')
 # A torch.Tensor is a multi-dimensional matrix containing elements of a single data type.
 # Calculations between tensors can only happen if the tensors share the same dtype.
 
 print('lets simply create a numpy array with value 1,2,3,4,5')
-
+arr = np.array([1,2,3,4,5])
+print(arr)
+print(arr.dtype)
+print(type(arr))
 
 print('now lets create a 4x3 2D array (matrix)')
-
-
+x = torch.tensor(arr)
+print(x)
+print(x.dtype)
+print(type(x))
+arr2 = np.arange(0,12).reshape(4,3)
+print(arr2)
 print('lets convert this 2D array into a torch tensor')
-
+x2 = torch.tensor(arr2)
+print(x2)
+print(x2.type())
 
 print('lets create a tensor from scratch')
 #   Uninitialized tensors with .empty()
-
-
+x = torch.empty(4,3)
+print(x)
+print(x.type())
 #   Initialized tensors with .zeros() and .ones()
-
-
+x = torch.zeros(4,3,dtype = torch.int32)
+print(x)
+x1 = torch.ones(4,3)
+print(x1)
 print('Tensors from ranges')
-
-
+x = torch.arange(0,9).reshape(3,3)
+print(x)
 print('Tensors from data')
-
-
+x = torch.tensor([1,2,3,4,5])
+print(x)
 print('Random number tensors that follow the input size')
-
-
+x48 = torch.randint(0,10,[4,3])
+print(x48)
+x50 = torch.randn(4,3)
+print(x50)
 print('Set random seed which allows us to share the same "random" results.')
-
-
+np.random.seed(1)
+torch.manual_seed(1)
+print(np.random.randint(0,5,4))
+print(torch.randint(0,5,[2,2]))
 print('Tensor attributes')
-
+print(x.shape)
+print(x.size())
+print(x.device)
 # PyTorch supports use of multiple devices, harnessing the power of one or more GPUs in addition to the CPU.
 # We won't explore that here, but you should know that operations between tensors can only happen for tensors installed on the same device.
--- 02_image_classification_with_pytorch_and_ANN/04_pytorch_operations.py
+++ 02_image_classification_with_pytorch_and_ANN/04_pytorch_operations.py
@@ -7,41 +7,50 @@
 # Additional, more advanced operations
 
 import torch
-import numpy as np
-
 print('Indexing and slicing')
 x = torch.arange(6).reshape(3,2)
 print(x)
 
 print('Grabbing the right hand column values')
-
+print(x[:,1])
 
 print('Grabbing the right hand column as a (3,1) slice')
-
-
+print(x[:,1].reshape(3,1))
+print(x[:,1].view(3,1))
 # view() and reshape() do essentially the same thing by returning a reshaped tensor
 # without changing the original tensor in place.
-
+x1 = torch.arange(9).reshape(3,3)
+print(x1)
+print(x1.shape)
+print(x.view(-1,6))
 
 print('Views can infer the correct size')
 
 
 print('Tensor basic operations')
-
-
+a = torch.tensor([1,2,3], dtype=torch.float) #ctrl+c
+b = torch.tensor([4,5,6], dtype=torch.float)
+a+b
 print('Changing a tensor in-place')
-
-
+a.add_(b)
+print(a)
 print('Dot products')
-
+a = torch.tensor([1,2,3], dtype=torch.float) #ctrl+c
+b = torch.tensor([4,5,6], dtype=torch.float)
+print(a.dot(b))
 
 print('Matrix multiplication')
-
-
+a = torch.tensor([[0,2,4],[1,3,5]])
+b = torch.tensor([[6,7],[8,9],[10,11]])
+print(a.size())
+print(b.size())
+print(torch.mm(a,b))
+print(a.mm(b))
 print('L2 or Euclidean Norm')
 # The L2 norm calculates the distance of the vector coordinate from the origin of the vector space.
 # As such, it is also known as the Euclidean norm as it is calculated as the Euclidean distance from the origin.
 # The result is a positive distance value.
-
+x = torch.tensor([2.,5.,8.,14.])#as float dtype, default is float
+print(x.norm())
 
 
